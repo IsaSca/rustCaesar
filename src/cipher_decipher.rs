7f6508c0b36de6f5193c198b9759d83effbd055e
Isaac Scarisbrick <isasca@hotmail.co.uk>
2020-08-12 10:34:23 +0100

    Added a more verbose main method.

--- src/cipher_decipher.rs
+++ src/cipher_decipher.rs
@@ -12,15 +12,15 @@ pub(crate) fn encipher(plaintext: &str, rotation: u8) -> String {
 }
 
 /*The only minute difference between this and the cipher is the logic on line 20.*/
-pub(crate) fn decipher(plaintext: &str, rotation: u8) -> String {
-    let mut ciphertext: Vec<char> = vec![' '; plaintext.len()];
-    let to_iter = plaintext.to_ascii_uppercase();
+pub(crate) fn decipher(ciphertext: &str, rotation: u8) -> String {
+    let mut plaintext: Vec<char> = vec![' '; ciphertext.len()];
+    let to_iter = ciphertext.to_ascii_uppercase();
     for(pos, character) in to_iter.chars().enumerate() {
-        ciphertext[pos] = if character <= 'Z' && character >= 'A' {
+        plaintext[pos] = if character <= 'Z' && character >= 'A' {
             (((((character as u8) - ('A' as u8)) + (26 - rotation)) % 26) + ('A' as u8)) as char
         } else {
             character
         };
     }
-    ciphertext.into_iter().collect()
+    plaintext.into_iter().collect()
 }
