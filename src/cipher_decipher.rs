e89fd82bd0216ab1c99221883d4a5860357250b4
Isaac Scarisbrick <isasca@hotmail.co.uk>
2020-08-12 10:06:32 +0100

    Better error handling and decipher added.

--- src/cipher_decipher.rs
+++ src/cipher_decipher.rs
@@ -1,9 +1,23 @@
 pub(crate) fn encipher(plaintext: &str, rotation: u8) -> String {
     let mut ciphertext: Vec<char> = vec![' '; plaintext.len()];
     let to_iter = plaintext.to_ascii_uppercase();
-    for(idx, character) in to_iter.chars().enumerate() {
-        ciphertext[idx] = if character <= 'Z' && character >= 'A' {
+    for(pos, character) in to_iter.chars().enumerate() {
+        ciphertext[pos] = if character <= 'Z' && character >= 'A' {
             (((((character as u8) - ('A' as u8)) + rotation) % 26) + ('A' as u8)) as char //if the character is less than A and more than Z, then rotate it
+        } else {
+            character
+        };
+    }
+    ciphertext.into_iter().collect()
+}
+
+/*The only minute difference between this and the cipher is the logic on line 20.*/
+pub(crate) fn decipher(plaintext: &str, rotation: u8) -> String {
+    let mut ciphertext: Vec<char> = vec![' '; plaintext.len()];
+    let to_iter = plaintext.to_ascii_uppercase();
+    for(pos, character) in to_iter.chars().enumerate() {
+        ciphertext[pos] = if character <= 'Z' && character >= 'A' {
+            (((((character as u8) - ('A' as u8)) + (26 - rotation)) % 26) + ('A' as u8)) as char
         } else {
             character
         };
